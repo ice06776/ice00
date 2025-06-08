18022ac7d0986b1e6ec31323e6bdfded562253d8
Li <ice06776@outlook.com>
2025-06-08 15:33:26 +0800

    init: pixel portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/pixel-portfolio/', // 仓库名，前后都要有斜杠
+    base: '/ice00/', // 仓库名，前后都要有斜杠
 })
