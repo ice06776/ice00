7f58f9359f6f12b4fa4594dc8b269c85cb9d1dbd
Li <ice06776@outlook.com>
2025-06-08 15:24:13 +0800

    init: pixel portfolio

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    base: '/pixel-portfolio/', // 仓库名，前后都要有斜杠
+})
